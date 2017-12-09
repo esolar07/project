f1fd2293cd24a2209b3f728354c19a9a2a791353
esolar <esolar07@gmail.com>
2017-12-09 11:01:40 -0500

    started modifing page layout

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,7 +1,6 @@
-<nav class="pa3 pa4-ns">
-    <a class="link dim black b f6 f5-ns dib mr3" href="{{url('/')}}" title="Home">Eddie S</a>
-    <a class="link dim gray f6 f5-ns dib mr3" href="{{url('/')}}" title="Home">Home</a>
-    <a class="link dim gray f6 f5-ns dib mr3" href="{{url('/about')}}" title="About">About</a>
-    <a class="link dim gray f6 f5-ns dib mr3" href="{{url('/work')}}" title="Work">Work</a>
-    <a class="link dim gray f6 f5-ns dib" href="{{url('/contact')}}" title="Contact">Contact</a>
+<nav class="menu">
+    <a class="menu__item" href="{{url('/')}}" title="Home">Home</a>
+    <a class="menu__item" href="{{url('/about')}}" title="About">About</a>
+    <a class="menu__item" href="{{url('/work')}}" title="Work">Work</a>
+    <a class="menu__item" href="{{url('/contact')}}" title="Contact">Contact</a>
 </nav>
