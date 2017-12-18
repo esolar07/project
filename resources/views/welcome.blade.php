e8dabe42b3fda965d0daf69d434b57462bc4abb3
esolar <esolar07@gmail.com>
2017-12-18 08:56:25 -0500

    working on grid layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,46 +5,8 @@
 
     <div class="l-welcome-container">
 
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box box__transparent"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box box__transparent"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box">Eddie Solar</div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box box__transparent"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box">Web</div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box">Developer</div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-
+        <section class="welcome-section__left">left</section>
+        <section class="selcome-section__middle">middle</section>
+        <section class="welcome-section__right">right</section>
     </div>
 @endsection
